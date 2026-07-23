4ff5bd08344057a7bf92b75e9055808d89b28f01
marwa-ahmed93 <https://github.com/marwa-ahmed93/Restaurant.git>
2026-07-23 15:16:04 +0300

    css Grid& php

---
+++ session_5/index.php
@@ -0,0 +1,128 @@
+<?php
+
+// echo "Hello Worl <br>" ;
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+// echo "Hello Worl <br>";
+
+
+// echo 5+5; //10
+
+/**Variables 
+ * int x=10
+ * x*20
+ */
+
+// $name = "marwa";  //string
+// $name = 10;       //integer
+// echo gettype($name);
+// echo $name;
+// echo "<br>";
+
+// $x = 5+5 ;  //10
+// echo $x*20;
+
+
+/**data type 
+ * int x = 5
+ * string
+ * number
+ * float
+ * array
+ * object
+ * null
+ */
+
+
+
+
+
+
+
+// $name = "ahmed";
+
+// echo "welcome ".$name;
+
+
+// $start = null ;
+//////////////////////////////////////////////////////////////////////////
+/**Casting
+ * 1-automatic
+ * 2-manual 
+ */
+
+// $number = "10" + 20;   //10+20=>30 automatic
+// echo $number ;
+
+
+// $number = (int)"marwa" + 20;   //0+20=>20 manual
+// echo $number ;
+
+
+// $number =  (int)5.5 +  20;   //520=>20 manual
+// echo $number ;
+
+
+////////////////////////////////////////////////////////
+/**if  */
+
+
+// $age = 18;
+// if($age >= 18){
+
+//     echo "welcome";
+// }
+
+// else{
+//   echo "sorry";
+// }
+
+// $gender = "femnnale";
+//  if($gender == "female"){
+//     echo "welcome female";
+//  }
+//  elseif($gender == "male"){
+//   echo "welcome male";
+//  }
+//  else{
+//     echo "invalid";
+//  }
+
+//nested if 
+$age = 10;
+$gender = "male";
+// if ($age > 18) {
+//     if ($gender == "female") {
+//         echo "enter";
+//     } else {
+//     }
+// } else {
+// }
+
+
+// if($age >18){
+//     echo "enter";
+// }
+// else{
+//     echo "not enter";
+// }
+
+// echo  $age >18 ? "enter" : "not enter" ;
+
+// if($gender == "female"){
+//     echo "hi female";
+// }
+// else{
+//     echo "hi male";
+// }
+
+if(' ')  echo  "true";
+else{
+    echo "false";
+}
